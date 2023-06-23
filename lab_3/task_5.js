554bd117a7be8cea0142d7ced023a74f555ff589
Shamplooo <segunsamur@gmail.com>
2023-06-23 16:00:15 +0300

    completed the task 5

--- lab_3/task_5.js
+++ lab_3/task_5.js
@@ -3,7 +3,8 @@
 Створити змінні «first» = 1, «second» = 2, «senseOfLife» = 42. 
 За допомогою методу масива .push() додати в масив спочатку «first» потім «second». 
 Після цього, за допомогою методу .unshift() додати змінну «senseOfLife».
- */
+*/
+
 
 let resultArray = [];
 let first = 1;
